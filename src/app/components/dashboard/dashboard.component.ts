37a914e706ad5385c88c637d6d3c5f791f2ab5a9
imranmr <imran.mohdraji@gmail.com>
2022-01-09 18:14:46 -0500

    now constantly updates dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+import { UserService } from './../../services/user.service';
 import { Router } from '@angular/router';
 import { DataserviceService } from 'src/app/services/dataservice.service';
 import { Component, OnInit } from '@angular/core';
@@ -8,14 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  olduser:any;
   user:any;
-  constructor(private data:DataserviceService, private router:Router) { }
+  constructor(private data:DataserviceService, private router:Router, private userservice:UserService) { }
   
   ngOnInit(): void {
-    this.data.currentuser.subscribe(data => this.user=data);
-    if(this.user=='null'){
+
+    
+
+    this.data.currentuser.subscribe(data => this.olduser=data);
+    if(this.olduser=='null'){
       this.router.navigate(['login']);
     }
+    let userdetail={
+      "userid":this.olduser.userid
+    }
+    this.userservice.getlatestuserdetail(userdetail).subscribe(
+      (res:any)=>{
+        this.user = res;
+      }
+    )
+
+
 
   }
   resetpassword(){
